fc7fb75613170456be1ebbf12f4f97699b2219ea
romangatiatullin <mr.artemka02@mail.ru>
2024-10-23 14:16:33 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -4,8 +4,8 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-       return first
+        return first if first > 0 else 1
 
 
-result = get_multiplied_digits(3045)
+result = get_multiplied_digits(402030)
 print(result)
